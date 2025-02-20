a315b2bc73bf1c353adc8ac7745668cf14af44ea
MD. Mehedi Hasan Rabbi <mehedi.rabbi@asa-international.com>
2025-02-20 16:26:45 +0600

    Add initial implementation of chatbot UI from morphic template

---
+++ projects/chatbot/frontend/chatbot/src/app/layout.tsx
@@ -0,0 +1,72 @@
+import type { Metadata, Viewport } from "next";
+import { Inter as FontSans } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import Header from "@/components/header";
+import { cn } from "@/lib/utils";
+import Footer from "@/components/footer";
+import { Toaster } from "@/components/ui/sonner";
+
+const fontSans = FontSans({
+  subsets: ['latin'],
+  variable: '--font-sans'
+})
+
+const title = 'Chtbot'
+const description =
+  'A fully open-source AI-powered chatbot with a generative UI.'
+
+export const metadata: Metadata = {
+  metadataBase: new URL('http://rabbi.work'),
+  title,
+  description,
+  openGraph: {
+    title,
+    description
+  },
+  twitter: {
+    title,
+    description,
+    card: 'summary_large_image',
+    creator: '@mehedi'
+  }
+}
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  minimumScale: 1,
+  maximumScale: 1
+}
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en"
+      // `next-themes` injects an extra classname to the body element to avoid
+      // visual flicker before hydration. Hence the `suppressHydrationWarning`
+      // prop is necessary to avoid the React hydration mismatch warning.
+      // https://github.com/pacocoursey/next-themes?tab=readme-ov-file#with-app
+      suppressHydrationWarning
+    >
+      <body className={cn('font-sans antialiased', fontSans.variable)}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Header />
+          {children}
+          {/* {enableSaveChatHistory && <Sidebar />} */}
+          <Footer />
+          <Toaster />
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ projects/chatbot/frontend/chatbot/src/app/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import { Chat } from "@/components/chat";
+// import { generateId } from 'ai'
+
+export default function Home() {
+  // const id = generateId()
+  return <Chat id={`1`} />;
+}
